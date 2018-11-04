dbb70732b7629037ca47cd932466d6f2aba17b9f
Lucas Theisen <lucastheisen@pastdev.com>
2018-11-04 14:25:59 -0500

    lets try this

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-set -e
-
-sudo apt-get update
-sudo apt-get install -y software-properties-common
-sudo apt-add-repository -y ppa:ansible/ansible
-
-sudo apt-get update
-sudo apt-get -y install ansible
-
-# Create .developer/dev-bootstrap/vars to use for ansible override
-WIN_HOME=$(wslpath $(cmd.exe /c "<nul set /p=%UserProfile%" 2>/dev/null))
-DOT_DEVELOPER="$WIN_HOME/.developer"
-mkdir -p "$DOT_DEVELOPER/dev-bootstrap/vars"
-if [ -e "$DOT_DEVELOPER" ]; then
-  ln -s "$DOT_DEVELOPER" ~/.developer
-fi
-
+cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+pwd
+ansible-playbook update.yml
